49605c9f7dea14f2ce8edb73c72537ef39b0a0bd
phantantaidv <50291768+phantantaidv@users.noreply.github.com>
2020-08-06 19:13:58 +0800

    Update consumer.py

--- consumer.py
+++ consumer.py
@@ -3,8 +3,58 @@ import pika
 import requests
 import time
 import socket
-import getLoadbalancerIP
-loadBalancerIp = getLoadbalancerIP.findLoadbalancerIp()
+import json
+# import getLoadbalancerIP
+# loadBalancerIp = getLoadbalancerIP.findLoadbalancerIp()
+
+TACKER_URL = 'http://192.168.1.111'
+
+
+def get_token():
+    # print("\nGet token:")
+    get_token_result = ''
+    get_token_url = TACKER_URL + ':5000/v3/auth/tokens'
+    get_token_body = {
+        'auth': {
+            'identity': {
+                'methods': [
+                    'password'
+                ],
+                'password': {
+                    'user': {
+                        'domain': {
+                            'name': 'Default'
+                        },
+                        'name': 'admin',
+                        'password': 'admin'
+                    }
+                }
+            },
+            'scope': {
+                'project': {
+                    'domain': {
+                        'name': 'Default'
+                    },
+                    'name': 'admin'
+                }
+            }
+        }
+    }
+    get_token_response = requests.post(get_token_url, data=json.dumps(get_token_body))
+    # print("Get Tacker token status: " + str(get_token_response.status_code))
+    get_token_result = get_token_response.headers['X-Subject-Token']
+    return get_token_result
+
+def findLoadbalancerIp():
+    getNetworkListUrl = TACKER_URL + ':9890/v1.0/vnfs'
+    token = get_token()
+    headers = {'X-Auth-Token': token}
+    getResponse = requests.get(getNetworkListUrl, headers=headers)
+    # print("Server Response status:" + str(getResponse.status_code))
+    getServerListResult = str(getResponse.json())
+    markPoint = getServerListResult.find("""'{"VDU5""")
+    LoadbalancerIp = getServerListResult[markPoint + 11: markPoint + 26]
+    return LoadbalancerIp
 
 while 1:
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -18,7 +68,7 @@ while 1:
         time.sleep(1)
 credentials = pika.PlainCredentials('openstack', 'rabbit')
 connection = pika.BlockingConnection(
-    pika.ConnectionParameters(loadBalancerIp, 5672, '/', credentials))
+    pika.ConnectionParameters(findLoadbalancerIp(), 5672, '/', credentials))
 
 channel = connection.channel()
 
