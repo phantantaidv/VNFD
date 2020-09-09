43ba0fc766455f14f954512954b935a103315791
phantantaidv <50291768+phantantaidv@users.noreply.github.com>
2020-09-09 23:00:22 +0800

    Update monitor_dss4G.py

--- monitor_dss4G.py
+++ monitor_dss4G.py
@@ -1,42 +1,28 @@
-import os
+import psutil
 import time
 import requests
-from subprocess import check_output
 from subprocess import check_output
 
 ips = check_output(['hostname', '--all-ip-addresses'])
 ip = str(ips)
 ip = ip[2:-4]
+print(len(ip))
 print("Instances's IP:", ip)
+
 ACK_message = "ACK"
 r = requests.post('http://192.168.1.111:9999/ACKmessage', data=ACK_message)
 print("<Response [{status_code}] {reason}>".format(status_code=r.status_code, reason=r.reason))
+i = 0
 
 while True:
-    CPU_Pct = round(float(
-        os.popen('''grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage }' ''').readline()),
-                        2)*3
-    CPU_Pct = str(CPU_Pct)
-
-    # print results
-    mem = str(os.popen('free -t -m').readlines())
-
-    T_ind = mem.index('M')
-
-    mem_G = mem[T_ind + 14:-4]
-    Total_mem = mem_G.split()[0]
-    Used_mem = mem_G.split()[1]
-    mem_U_in_Percent = (int(Used_mem) / int(Total_mem)) * 100
-    mem_U_in_Percent = round(mem_U_in_Percent, 2)
-    print("*******************************")
-    print("CPU Usage = " + CPU_Pct, "%")
-    print("Total mem:", Total_mem, "Mb")
-    print("Used mem:", Used_mem, "Mb")
-    print('Mem usage in percent:', mem_U_in_Percent, "%")
-    print("*******************************")
+    if i != 0:
+        CPU_Pct = round(psutil.cpu_percent()*4,2)
+        CPU_Pct = str(CPU_Pct)
+        print("*******************************")
+        print("CPU Usage = " + CPU_Pct, "%")
+        print("*******************************")
+        payload = "{ip}, {CPU_Pct}".format(ip=ip, CPU_Pct=CPU_Pct)
+        r = requests.post('http://192.168.1.111:9999/Instances', data=payload)
+        print("<Response [{status_code}] {reason}>".format(status_code=r.status_code, reason=r.reason))
+    i += 1
     time.sleep(1)
-    # print(type(CPU_Pct))
-    payload = "{ip}, {CPU_Pct}".format(ip=ip, CPU_Pct=CPU_Pct)
-    r = requests.post('http://192.168.1.111:9999/Instances', data=payload)
-    # print("<Response [{a}]>".format(a=r.status_code))
-    print("<Response [{status_code}] {reason}>".format(status_code=r.status_code, reason=r.reason))
